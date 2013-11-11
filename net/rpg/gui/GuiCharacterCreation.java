44c25beb4051d0101c6afa2c4c217dee8df1536d
The_SlayerMC <brock.kerley@hotmail.com>
2013-11-10 17:13:08 -0800

    update

--- net/rpg/gui/GuiCharacterCreation.java
+++ net/rpg/gui/GuiCharacterCreation.java
@@ -1,6 +1,7 @@
 package net.rpg.gui;
 
 import net.minecraft.client.gui.GuiScreen;
+import net.minecraft.util.EnumChatFormatting;
 import net.minecraft.util.ResourceLocation;
 import net.rpg.Reference;
 import net.rpg.Util;
@@ -11,38 +12,26 @@ import cpw.mods.fml.relauncher.Side;
 import cpw.mods.fml.relauncher.SideOnly;
 
 @SideOnly(Side.CLIENT)
-public class GuiCharacterCreation extends GuiScreen
-{
+public class GuiCharacterCreation extends GuiScreen {
 	
     protected int xSize = 176;
     protected int ySize = 166;
 	
 	private static final ResourceLocation texture = new ResourceLocation(Reference.RESOURCE_PREFIX + "/new_player");
 	
-	public GuiCharacterCreation()
-	{
-		
+	public GuiCharacterCreation() {
 		Util.info("Whats up?");
-		
 	}
 	
-	protected void drawGuiContainerForegroundLayer(int par1, int par2)
-	{
-		
-		this.fontRenderer.drawString("Choose your race:", 8, 6, 4210752);
-		
-		
+	protected void drawGuiContainerForegroundLayer(int par1, int par2) {	
+		this.fontRenderer.drawString(EnumChatFormatting.BOLD + "Choose your race:", 8, 6, 4210752);
 	}
 	
-	protected void drawGuiContainerBackgroundLayer(float par1, int par2, int par3)
-	{
-		
+	protected void drawGuiContainerBackgroundLayer(float par1, int par2, int par3) {
         GL11.glColor4f(1.0F, 1.0F, 1.0F, 1.0F);
         this.mc.getTextureManager().bindTexture(texture);
 	    int k = (this.width - this.xSize) / 2;
 	    int l = (this.height - this.ySize) / 2;
 	    this.drawTexturedModalRect(k, l, 0, 0, this.xSize, this.ySize);
-        
-	}
-	
+	}	
 }
--- net/rpg/gui/GuiStats.java
+++ net/rpg/gui/GuiStats.java
@@ -16,152 +16,97 @@ import org.lwjgl.opengl.GL11;
 
 public class GuiStats extends GuiScreen
 {
-	
+
 	private int coolDown;
-    private int xSize = 176;
-    private int ySize = 166;
+	private int xSize = 176;
+	private int ySize = 166;
+
+	private EntityPlayer player;
+
+	GuiButton guibutton;
 	
-    private EntityPlayer player;
-    
 	private static final ResourceLocation texture = new ResourceLocation(Reference.RESOURCE_PREFIX + "/stats");
-	
-	public GuiStats(EntityPlayer player)
-	{
-		
+
+	public GuiStats(EntityPlayer player) {
 		this.player = player;
-		
 	}
-	
+
 	@Override
-    public void initGui()
-    {
-		
-        this.buttonList.clear();
-        
-        if (this.mc.theWorld.getWorldInfo().isHardcoreModeEnabled())
-        {
-        	
-            if (this.mc.isIntegratedServerRunning())
-            {
-            	
-                this.buttonList.add(new GuiButton(1, this.width / 2 - 100, this.height / 4 + 96, I18n.getString("deathScreen.deleteWorld")));
-                
-            }
-            else
-            {
-            	
-                this.buttonList.add(new GuiButton(1, this.width / 2 - 100, this.height / 4 + 96, I18n.getString("deathScreen.leaveServer")));
-                
-            }
-            
-        }
-        else
-        {
-        	
-            this.buttonList.add(new GuiButton(1, this.width / 2 - 100, this.height / 4 + 72, I18n.getString("deathScreen.respawn")));
-            this.buttonList.add(new GuiButton(2, this.width / 2 - 100, this.height / 4 + 96, I18n.getString("deathScreen.titleScreen")));
+	public void initGui() {
+		this.buttonList.clear();
 
-            if (this.mc.getSession() == null)
-            {
-            	
-                ((GuiButton)this.buttonList.get(1)).enabled = false;
-                
-            }
-            
-        }
+		if (this.mc.theWorld.getWorldInfo().isHardcoreModeEnabled()) {
+			
+			if (this.mc.isIntegratedServerRunning()) {
+				this.buttonList.add(new GuiButton(1, this.width / 2 - 100, this.height / 4 + 96, I18n.getString("deathScreen.deleteWorld")));
+				
+			} else {
+				this.buttonList.add(new GuiButton(1, this.width / 2 - 100, this.height / 4 + 96, I18n.getString("deathScreen.leaveServer")));   
+			}
 
-        GuiButton guibutton;
+		} else {
+			this.buttonList.add(new GuiButton(1, this.width / 2 - 100, this.height / 4 + 72, I18n.getString("deathScreen.respawn")));
+			this.buttonList.add(new GuiButton(2, this.width / 2 - 100, this.height / 4 + 96, I18n.getString("deathScreen.titleScreen")));
+			
+			if (this.mc.getSession() == null) {
+				((GuiButton)this.buttonList.get(1)).enabled = false;               
+			}            
+		}
 
-        for (Iterator iterator = this.buttonList.iterator(); iterator.hasNext(); guibutton.enabled = false)
-        {
-        	
-            guibutton = (GuiButton)iterator.next();
-            
-        }
-        
-    }
-	
+		for (Iterator iterator = this.buttonList.iterator(); iterator.hasNext(); guibutton.enabled = false) {     	
+			guibutton = (GuiButton)iterator.next();           
+		} 
+	}
+
 	@Override
 	protected void keyTyped(char par1, int par2)
-	{
-		
-		
-		
-	}
-	
-    @Override
-    public void drawScreen(int par1, int par2, float par3)
-    {
-    	
-        this.drawGradientRect(0, 0, this.width, this.height, 1615855616, -1602211792);
-        GL11.glPushMatrix();
-        GL11.glScalef(2.0F, 2.0F, 2.0F);
-        this.drawCenteredString(this.fontRenderer, EnumChatFormatting.YELLOW + "Race: ", this.width / 2 / 2, 30, 16777215);
-        GL11.glPopMatrix();
-        
-        //this.drawCenteredString(this.fontRenderer, I18n.getString("deathScreen.hardcoreInfo"), this.width / 2, 144, 16777215);
-        //this.drawCenteredString(this.fontRenderer, I18n.getString("deathScreen.score") + ": " + EnumChatFormatting.YELLOW + this.mc.thePlayer.getScore(), this.width / 2, 100, 16777215);
-        
-        super.drawScreen(par1, par2, par3);
-        
-    }
-	
-    @Override
-    protected void actionPerformed(GuiButton button)
-    {
-    	
-        switch(button.id)
-        {
-        
-        case(1):
-        {
-        	
-        	this.mc.thePlayer.respawnPlayer();
-            this.mc.displayGuiScreen((GuiScreen)null);
-            break;
-        	
-        }
-        case(2):
-        {
-        	
-        	this.mc.theWorld.sendQuittingDisconnectingPacket();
-            this.mc.loadWorld((WorldClient)null);
-            this.mc.displayGuiScreen(new GuiMainMenu());
-        	
-        }
-        
-        }
-        
-    }
-    
-    @Override
-    public void updateScreen()
-    {
-        super.updateScreen();
-        
-        this.coolDown++;
-        GuiButton guibutton;
+	{}
 
-        if (this.coolDown == 20)
-        {
-        	
-            for (Iterator iterator = this.buttonList.iterator(); iterator.hasNext(); guibutton.enabled = true)
-            {
-            	
-                guibutton = (GuiButton)iterator.next();
-                
-            }
-            
-        }
-        
-    }
-	
-    @Override
-    public boolean doesGuiPauseGame()
-    {
-    	
-        return true;
-        
-    }
-    
+	@Override
+	public void drawScreen(int par1, int par2, float par3) {
+		this.drawGradientRect(0, 0, this.width, this.height, 1615855616, -1602211792);
+		GL11.glPushMatrix();
+		GL11.glScalef(2.0F, 2.0F, 2.0F);
+		this.drawCenteredString(this.fontRenderer, EnumChatFormatting.YELLOW + "Race: ", this.width / 2 / 2, 30, 16777215);
+		GL11.glPopMatrix();
+
+		//this.drawCenteredString(this.fontRenderer, I18n.getString("deathScreen.hardcoreInfo"), this.width / 2, 144, 16777215);
+		//this.drawCenteredString(this.fontRenderer, I18n.getString("deathScreen.score") + ": " + EnumChatFormatting.YELLOW + this.mc.thePlayer.getScore(), this.width / 2, 100, 16777215);
+
+		super.drawScreen(par1, par2, par3);
+	}
+
+	@Override
+	protected void actionPerformed(GuiButton button) {
+		
+		switch(button.id){
+		case(1): {
+			this.mc.thePlayer.respawnPlayer();
+			this.mc.displayGuiScreen((GuiScreen)null);
+			break;
+		}
+		case(2): {
+			this.mc.theWorld.sendQuittingDisconnectingPacket();
+			this.mc.loadWorld((WorldClient)null);
+			this.mc.displayGuiScreen(new GuiMainMenu());
+		}
+	}
+}
+
+	@Override
+	public void updateScreen() {
+		super.updateScreen();
+		this.coolDown++;
+
+		if(this.coolDown == 20) {
+			for(Iterator iterator = this.buttonList.iterator(); iterator.hasNext(); guibutton.enabled = true) {
+				guibutton = (GuiButton)iterator.next();
+			}
+		}
+	}
+
+	@Override
+	public boolean doesGuiPauseGame() {
+		return true;
+	}
 }
