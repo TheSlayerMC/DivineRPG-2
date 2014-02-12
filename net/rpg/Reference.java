181e2e4dec7d266dc3f8c4d5ad47d451648262ea
SoccerStud <geekclb@gmail.com>
2014-02-12 15:58:39 -0500

    Inproved GUI overlay

--- net/rpg/Reference.java
+++ net/rpg/Reference.java
@@ -5,7 +5,7 @@ import java.util.Random;
 public class Reference {
 	public static final String MOD_ID = "rpg";
 	public static final String MOD_NAME = "DivineRPG 2";
-	public static final String MOD_VERSION = "1.0";
+	public static final String MOD_VERSION = "2.0";
 	public static final String CLIENT_PROXY = "net.rpg.ClientProxy";
 	public static final String SERVER_PROXY = "net.rpg.ServerProxy";
 	public static final String RESOURCE_PREFIX = "RPG:";
--- net/rpg/gui/GuiVersion.java
+++ net/rpg/gui/GuiVersion.java
@@ -9,20 +9,28 @@ import net.rpg.helper.DataHelper;
 
 public class GuiVersion {
 	
+	
 	public static void draw() {
 		FontRenderer fontRenderer = Minecraft.getMinecraft().fontRenderer;
-		String name = Util.DARK_GREEN + Reference.MOD_NAME;
-		String version = Util.GREEN + "Version: " + Reference.MOD_VERSION;
-		fontRenderer.drawString(name, 2, 2, 4210752);
-		fontRenderer.drawString(version, 2, 15, 4210752);
+		String name = Util.AQUA + Reference.MOD_NAME;
+		String version = Util.DARK_PURPLE + "Version: " + Reference.MOD_VERSION;
 		String credits;
 		if(RPG.credits == 0)
-			credits = Util.BLUE + "Credits: " + Util.WHITE + RPG.credits;
-		else if(RPG.credits < 30)
-			credits = Util.BLUE + "Credits: " + Util.WHITE + RPG.credits;
+			credits = Util.DARK_RED + "Credits: " + Util.DARK_RED + RPG.credits;
+		else if(RPG.credits < 50)
+			credits = Util.DARK_RED + "Credits: " + Util.RED + RPG.credits;
+		else if(RPG.credits < 100)
+			credits = Util.DARK_RED + "Credits: " + Util.GOLD + RPG.credits;
+		else if (RPG.credits < 500)
+			credits = Util.DARK_RED + "Credits: " + Util.YELLOW + RPG.credits;
+		else if (RPG.credits < 1000)
+			credits = Util.DARK_RED + "Credits: " + Util.AQUA + RPG.credits;
 		else
-			credits = Util.BLUE + "Credits: " + Util.WHITE + RPG.credits;
+			credits = Util.DARK_RED + "Credits: " + Util.GREEN + RPG.credits;
 		String s = credits;
-		fontRenderer.drawString(s, 2, 30, 0xFFFFFF);
+		
+		fontRenderer.drawString(name, 2, 2, 4210752);
+		fontRenderer.drawString(version, 2, 12, 4210752);
+		fontRenderer.drawString(s, 2, 22, 0xFFFFFF);
 	}
 }
